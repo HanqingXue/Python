f74a0ab029019d120a1cf96750fd0ae90202e42e
MYHITGITBIT <1120310228@hit.edu.cn>
2017-11-17 22:57:44 +0800

    add the error page

--- MicroAI/handlers/result.py
+++ MicroAI/handlers/result.py
@@ -5,5 +5,13 @@ import tornado.web
 class ResultHandler(tornado.web.RequestHandler):
 	def post(self):
 		algorithm = self.get_argument('algorithms')
+		trainSample = self.get_argument('trainSample')
+		trainLabel = self.get_argument('trainLabel')
+		#testSample 
+		if len(trainSample)  == 0 or  len(trainLabel) == 0:
+			self.render("error.html")
+
 		print algorithm
+		print trainSample
+		print trainLabel
 		self.render('result.html', algo = algorithm)
