b58ad1f1b6ec34725c9264c88e66245ea021fb39
MYHITGITBIT <1120310228@hit.edu.cn>
2017-11-17 22:23:55 +0800

    Reformat the code of home page

--- MicroAI/handlers/result.py
+++ MicroAI/handlers/result.py
@@ -4,4 +4,6 @@ import tornado.web
 
 class ResultHandler(tornado.web.RequestHandler):
 	def post(self):
-		self.render('index.html')
+		algorithm = self.get_argument('algorithms')
+		print algorithm
+		self.render('result.html', algo = algorithm)
